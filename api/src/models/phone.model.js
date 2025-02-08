975b39d69c4eeef7e9f8a163be376abd3403dc94
Script Versity <scriptversity@yahoo.com>
2025-02-07 22:14:49 -0500

    phone model tweaked

--- api/src/models/phone.model.js
+++ api/src/models/phone.model.js
@@ -6,6 +6,10 @@ const phoneModel = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      ownerType: {
+        type: DataTypes.ENUM("user", "business", "deliverer"), // To know who owns the phone
+        allowNull: false,
+      },
     },
     {
       timestamps: false,
@@ -16,3 +20,22 @@ const phoneModel = (sequelize, DataTypes) => {
 };
 
 export default phoneModel;
+
+// const phoneModel = (sequelize, DataTypes) => {
+//   const Phone = sequelize.define(
+//     "phone",
+//     {
+//       number: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//     },
+//     {
+//       timestamps: false,
+//     }
+//   );
+
+//   return Phone;
+// };
+
+// export default phoneModel;
