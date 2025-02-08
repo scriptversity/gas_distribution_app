086831a2ad429559c058e3b7d9452f9662fca4b5
Script Versity <scriptversity@yahoo.com>
2025-02-07 22:02:15 -0500

    index.js file tweaked

--- api/index.js
+++ api/index.js
@@ -4,8 +4,12 @@ import { conn } from "./src/database/db.js";
 
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log("server listening at " + port);
-  });
-});
+conn
+  .sync({ force: true })
+  .then(() => console.log("Database synced successfully"))
+  .then(() => {
+    server.listen(port, () => {
+      console.log("server listening at " + port);
+    });
+  })
+  .catch((error) => console.log(error));
