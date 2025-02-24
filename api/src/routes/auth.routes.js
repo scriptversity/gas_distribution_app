5902ca70d93820e0a018c95573b1aad60d8134e9
Script Versity <scriptversity@yahoo.com>
2025-02-23 23:11:18 -0500

    auth.routes modified

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { loginHandler } from "../handlers/auth/loginHandler.js";
 import { authHandler } from "../handlers/auth/authHandler.js";
-const router = Router();
-
+import { logoutHandler } from "../handlers/auth/logoutHandler.js";
+import { tokenRefreshHandler } from "../handlers/auth/tokenRefreshHandler.js";
 import auth from "../middlewares/auth.js";
+
+const router = Router();
 
 // @route   POST api/v1/user/login
 // @desc    Login user & return JWT token
@@ -14,5 +16,15 @@ router.route("/user/login").post(loginHandler);
 // @desc    Get current user by token
 // @access  Private
 router.route("/user/auth").get(auth, authHandler);
+
+// @route   POST api/v1/user/logout
+// @desc    Logout user & invalidate JWT token
+// @access  Private
+router.route("/user/logout").post(auth, logoutHandler);
+
+// @route   POST api/v1/user/refresh
+// @desc    Refresh user access token
+// @access  Private
+router.route("/user/refresh").post(auth, tokenRefreshHandler);
 
 export default router;
